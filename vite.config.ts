274e45f2146ef12194ed93d22f858139ace87aa1
y4ssn <yssvn34@gmail.com>
2025-11-24 21:33:03 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    target: 'esnext'
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      // Stringify the API key to inject it into the code during build
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+    build: {
+      outDir: 'dist',
+      target: 'esnext'
+    }
   }
 })
